b265666b029f682fe6440d3fbe326b8b4e4f4383
Nathan Tarr <nmtarr@ncsu.edu>
2020-06-15 11:28:35 -0400

    Completed draft.

--- rebird-test.R
+++ rebird-test.R
@@ -6,15 +6,19 @@ library("rebird")
 county <- "US-OR-047"
 startyr <- 2015
 birders_list <- "/users/nmtarr/Documents/eBirding/EdCoreyBirds.csv"
-
+ß
 # get the birder's spp list ---------------------------------------------------
 ed <- read_csv(birders_list)
 # WORK HERE reformat to match species list from freq
 lifelist <- unique(ed$Species) %>%
-  str_split(" - ", n = 2)
+  str_split(" - ", simplify = TRUE) %>%
+  .[,1]
 
 # get frequency data from ebird -----------------------------------------------
 freq <- ebirdfreq(loctype = "counties", loc = county, startyear = startyr)
-spplist <- unique(freq$comName)
+sitelist <- unique(freq$comName)
 
 # find spp that would be lifers -----------------------------------------------
+new_lifers <- setdiff(sitelist, lifelist)
+new_life_df <- filter(freq, comName %in% new_lifers)
+write_excel_csv(new_life_df, "/users/nmtarr/Documents/eBirding/EdCorey_new_spp.csv")
